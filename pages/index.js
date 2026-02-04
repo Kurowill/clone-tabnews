bbf802e9a9b5f360abc00b2e708909c718f2cf25
Kurowill <53104299+Kurowill@users.noreply.github.com>
2026-02-04 18:11:38 +0000

    adiciona scripts `lint:check`e `lint.fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Mary, meu amor, te amor muito! Se você me ama, dê uma risadinha! rs 😍</h1>;
-    }
+  return (
+    <h1>
+      Mary, meu amor, te amor muito! Se você me ama, dê uma risadinha! rs 😍
+    </h1>
+  );
+}
 
 export default Home;
